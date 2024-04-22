a0620495392d9ccae1b8164c0372b6b41724da41
Jennifer-Sence <sencejenni@gmail.com>
2024-04-22 19:26:42 +0100

    sims.txt

--- src/Main.java
+++ src/Main.java
@@ -9,11 +9,28 @@ import OutrasClasses.Sims;
 import Personagens.Jogador;
 import Personagens.Pessoa;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
 
+ public static void imprimirFicheiro(String path) throws FileNotFoundException {
+  // Instanciar um Scanner para o Ficheiro que foi passado por parâmetro
+  Scanner leitorFicheiro = new Scanner(new File(path));
+  //percorrer o ficheiro, guardar as linhas do ficheiro na variável linha
+  while (leitorFicheiro.hasNextLine()) {
+   String linha = leitorFicheiro.nextLine();
+   //imprimir todas as linhas do ficheiro
+   System.out.println(linha);
+  }
+ }
+
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+      imprimirFicheiro("src/Files/sims4.txt");
         System.out.println();
        Pessoa p1 = Sims.criarPessoa();
         Sims.visitarPropriedades(p1);
